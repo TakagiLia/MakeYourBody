36f18e77b176d6059bf5634fa79a18e71a52b6a3
TakagiLia <r.takagi.test@gmail.com>
2023-06-18 17:46:26 +0900

    ActivityにLoginFragmentを埋め込んでログインページを表示

--- app/src/main/java/com/example/makeyourbody/MainActivity.kt
+++ app/src/main/java/com/example/makeyourbody/MainActivity.kt
@@ -2,10 +2,6 @@ package com.example.makeyourbody
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-import androidx.databinding.DataBindingUtil
-import com.example.makeyourbody.databinding.ActivityMainBinding
-import com.example.makeyourbody.ui.theme.MakeYourBodyTheme
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
