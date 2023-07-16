5c08d16a2420e997f5a14fdafbddd629e4f531ef
TakagiLia <r.takagi.test@gmail.com>
2023-07-16 18:57:31 +0900

    日付ダイアログのフラグメントの新規追加

---
+++ app/src/main/java/com/example/makeyourbody/DatePickerFragment.kt
@@ -0,0 +1,12 @@
+package com.example.makeyourbody
+
+import android.app.DatePickerDialog
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener  {
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+    }
+}
