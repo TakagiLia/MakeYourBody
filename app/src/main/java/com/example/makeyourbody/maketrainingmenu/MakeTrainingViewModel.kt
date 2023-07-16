ba62357be4fe350b8d3a55d419cb658b8b74e6b4
TakagiLia <r.takagi.test@gmail.com>
2023-07-16 19:15:12 +0900

    セッション日と対象ユーザの値をViewモデルで保持

--- app/src/main/java/com/example/makeyourbody/maketrainingmenu/MakeTrainingViewModel.kt
+++ app/src/main/java/com/example/makeyourbody/maketrainingmenu/MakeTrainingViewModel.kt
@@ -1,7 +1,23 @@
 package com.example.makeyourbody.maketrainingmenu
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MakeTrainingViewModel : ViewModel()  {
+
+    //トレーニング作成画面　セッション日付
+    private val _menuDate = MutableLiveData<String>()
+    val menuDate get() = _menuDate
+
+    //トレーニング作成画面　対象ユーザ
+    private val _menuTargetUser = MutableLiveData<String>()
+    val menuTargetUser get() = _menuTargetUser
+
+    fun setMenuDate(Date :String){
+        _menuDate.value = Date
+    }
+    fun setTargetUser(targetUser :String){
+        _menuTargetUser.value = targetUser
+    }
 
 }
