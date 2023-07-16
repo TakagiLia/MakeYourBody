241adf951b3d919c1391da833aba9c9f0a85e5d0
TakagiLia <r.takagi.test@gmail.com>
2023-07-16 17:56:43 +0900

    メニュー作成画面のロゴ表示

--- app/src/main/java/com/example/makeyourbody/maketrainingmenu/MakeTrainingFragment.kt
+++ app/src/main/java/com/example/makeyourbody/maketrainingmenu/MakeTrainingFragment.kt
@@ -1,7 +1,37 @@
 package com.example.makeyourbody.maketrainingmenu
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.makeyourbody.databinding.FragmentMakeTrainingBinding
 
-class MakeTrainingFragment : Fragment(){
+class MakeTrainingFragment : Fragment() {
+
+    private var _binding: FragmentMakeTrainingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        Log.d("[TopPageFragment]", "onCreateView()")
+        _binding = FragmentMakeTrainingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        Log.d("[TopPageFragment]", "onDestroyView()")
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
