0ba987bc1fddd212e86dc5ce2798f3ebc491f7ca
TakagiLia <r.takagi.test@gmail.com>
2023-07-16 19:06:30 +0900

    日付選択ボタンで日付を選択できるように改修

--- app/src/main/java/com/example/makeyourbody/maketrainingmenu/MakeTrainingFragment.kt
+++ app/src/main/java/com/example/makeyourbody/maketrainingmenu/MakeTrainingFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.makeyourbody.DatePickerFragment
 import com.example.makeyourbody.databinding.FragmentMakeTrainingBinding
 
 class MakeTrainingFragment : Fragment() {
@@ -15,6 +16,20 @@ class MakeTrainingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //日付のダイアログ表示
+        binding.menuDateBtn.setOnClickListener {
+
+            DatePickerFragment().show(childFragmentManager, DatePickerFragment::class.java.name)
+
+            //日付ダイアログでOK押下の際DatePickerFragmentからの値受け取り
+            childFragmentManager.setFragmentResultListener(
+                "request_key",
+                viewLifecycleOwner
+            ) { _, result: Bundle ->
+                binding.menuDateEdit.setText(result.getString("date_picker_value"))
+            }
+        }
 
     }
 
