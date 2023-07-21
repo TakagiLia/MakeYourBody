37b9bdafda7eeb5b0cb83c77de3c125eb2a280f0
TakagiLia <r.takagi.test@gmail.com>
2023-07-21 19:42:20 +0900

    スケジュール画面のリサイクラービューの実装

--- app/src/main/java/com/example/makeyourbody/schedule/schedulelist/ScheduleListViewHolder.kt
+++ app/src/main/java/com/example/makeyourbody/schedule/schedulelist/ScheduleListViewHolder.kt
@@ -1,4 +1,17 @@
 package com.example.makeyourbody.schedule.schedulelist
 
-class ScheduleListViewHolder {
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.makeyourbody.R
+
+class ScheduleListViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    val trainingdate = itemView.findViewById<TextView>(R.id.training_date)
+
+    val training_target = itemView.findViewById<TextView>(R.id.training_target)
+
+    val trainingDetailBtn = itemView.findViewById<Button>(R.id.training_details_btn)
+
 }
