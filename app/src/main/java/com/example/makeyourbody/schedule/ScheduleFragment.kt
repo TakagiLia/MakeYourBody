f49143eb3f7b37c3ebd49b8018c26b7d780d2715
TakagiLia <r.takagi.test@gmail.com>
2023-07-21 19:18:27 +0900

    スケジュール表示画面 レコード絞り込むための日付入力欄の実装

--- app/src/main/java/com/example/makeyourbody/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/example/makeyourbody/schedule/ScheduleFragment.kt
@@ -5,9 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.makeyourbody.DatePickerFragment
 import com.example.makeyourbody.databinding.FragmentScheduleBinding
 
-class ScheduleFragment :Fragment() {
+class ScheduleFragment : Fragment() {
 
     private var _binding: FragmentScheduleBinding? = null
     private val binding get() = _binding!!
@@ -15,6 +16,19 @@ class ScheduleFragment :Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //日付ボタン
+        binding.scheduleDateBtn.setOnClickListener {
+            //日付のダイアログ表示
+            DatePickerFragment().show(childFragmentManager, DatePickerFragment::class.java.name)
+
+            //日付ダイアログでOK押下の際DatePickerFragmentからの値受け取り
+            childFragmentManager.setFragmentResultListener(
+                "request_key",
+                viewLifecycleOwner
+            ) { _, result: Bundle ->
+                binding.scheduleDateEdit.setText(result.getString("date_picker_value"))
+            }
+        }
     }
 
     override fun onCreateView(
