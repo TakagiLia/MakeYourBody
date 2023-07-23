d20459591ba16aaea314bb1afbf1bd250b9bfb49
TakagiLia <r.takagi.test@gmail.com>
2023-07-23 17:10:14 +0900

    メニュー詳細画面のロゴ表示

--- app/src/main/java/com/example/makeyourbody/view/menudetails/MenuDetailsFragment.kt
+++ app/src/main/java/com/example/makeyourbody/view/menudetails/MenuDetailsFragment.kt
@@ -1,6 +1,33 @@
 package com.example.makeyourbody.view.menudetails
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.makeyourbody.databinding.FragmentMenuDetailsBinding
 
 class MenuDetailsFragment :Fragment() {
+
+    private var _binding: FragmentMenuDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMenuDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
