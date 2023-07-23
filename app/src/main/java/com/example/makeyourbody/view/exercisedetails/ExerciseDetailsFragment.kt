f570f0086c9ab92edb35a53f271fc232883cd561
TakagiLia <r.takagi.test@gmail.com>
2023-07-23 23:47:08 +0900

    種目詳細画面の表示項目に種目レコードの内容が反映されるように修正

--- app/src/main/java/com/example/makeyourbody/view/exercisedetails/ExerciseDetailsFragment.kt
+++ app/src/main/java/com/example/makeyourbody/view/exercisedetails/ExerciseDetailsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.makeyourbody.databinding.FragmentExerciseDetailsBinding
 
 
@@ -13,8 +14,12 @@ class ExerciseDetailsFragment : Fragment() {
     private var _binding: FragmentExerciseDetailsBinding? = null
     private val binding get() = _binding!!
 
+    private val exerciseDetailsViewModel: ExerciseDetailsViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.exerciseDetailsName.setText(exerciseDetailsViewModel.trainingItem.value?.name)
+        binding.exerciseDetailsContent.setText(exerciseDetailsViewModel.trainingItem.value?.detail)
     }
 
     override fun onCreateView(
