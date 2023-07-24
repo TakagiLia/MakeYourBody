8a05b11408697c612ab9a6cef85a12f58aed939d
Lea <92248493+TakagiLia@users.noreply.github.com>
2023-07-24 22:36:45 +0900

    種目詳細ページの新規追加 (#24)

--- app/src/main/java/com/example/makeyourbody/view/menudetails/MenuDetailsFragment.kt
+++ app/src/main/java/com/example/makeyourbody/view/menudetails/MenuDetailsFragment.kt
@@ -1,13 +1,18 @@
 package com.example.makeyourbody.view.menudetails
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.makeyourbody.NiftyCloudApiClient
+import com.example.makeyourbody.R
+import com.example.makeyourbody.data.TrainingItem
 import com.example.makeyourbody.databinding.FragmentMenuDetailsBinding
+import com.example.makeyourbody.view.exercisedetails.ExerciseDetailsViewModel
 
 class MenuDetailsFragment :Fragment() {
 
@@ -17,16 +22,26 @@ class MenuDetailsFragment :Fragment() {
     //トレーニングメニュー詳細情報保持用
     private val menuDetailsViewModel: MenuDetailsViewModel by activityViewModels()
 
+    //トレーニングアイテム詳細ページ表示用
+    private val exerciseDetailsViewModel: ExerciseDetailsViewModel by activityViewModels()
+
+    //リストアイテム詳細ページ遷移
+    private val onClickInfoBtn: (TrainingItem)-> Unit = { trainingItem ->
+        Log.d("--onClickInfoBtn--","詳細ボタン押下")
+        exerciseDetailsViewModel.setItem(trainingItem)
+        findNavController().navigate(R.id.action_menu_details_to_exercise_details_screen)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val fixingList = NiftyCloudApiClient().trainingItemFixList(menuDetailsViewModel.menu.value?.menuContent.toString())
+        val fixingList =
+            NiftyCloudApiClient().trainingItemFixList(menuDetailsViewModel.menu.value?.menuContent.toString())
         binding.apply {
             menuDetailsDate.setText(menuDetailsViewModel.menu.value?.menuDate)
             menuDetailsTarget.setText(menuDetailsViewModel.menu.value?.menuTarget)
             var menuDetailsItemList = NiftyCloudApiClient().getTrainingItemBindingMenu(fixingList) ?: emptyList()
-            menuDetailsList.adapter = MenuDetailsListAdapter(menuDetailsItemList)
+            menuDetailsList.adapter = MenuDetailsListAdapter(menuDetailsItemList,onClickInfoBtn)
         }
-
     }
 
     override fun onCreateView(
