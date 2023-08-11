e8a85f1b1126c6b3d9de24638dc25ab29c35647a
TakagiLia <r.takagi.test@gmail.com>
2023-08-11 19:02:54 +0900

    種目マスタの登録ボタン押下後の保存処理追加

--- app/src/main/java/com/example/makeyourbody/view/exercise/MakeExerciseFragment.kt
+++ app/src/main/java/com/example/makeyourbody/view/exercise/MakeExerciseFragment.kt
@@ -6,15 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.makeyourbody.databinding.FragmentMakeExerciseBinding
 import androidx.fragment.app.Fragment
+import com.example.makeyourbody.NiftyCloudApiClient
 
-class MakeExerciseFragment :Fragment() {
+class MakeExerciseFragment : Fragment() {
 
-    private var _binding:  FragmentMakeExerciseBinding? = null
+    private var _binding: FragmentMakeExerciseBinding? = null
 
     private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            exerciseRegisterBtn.setOnClickListener {
+                NiftyCloudApiClient().saveExercise(
+                    exerciseNameEdit.text.toString(),
+                    exerciseContentEdit.text.toString()
+                )
+            }
+
+        }
 
     }
 
