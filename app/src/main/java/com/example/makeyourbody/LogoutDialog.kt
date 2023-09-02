2e0a33a550d928821f3f04a5cc39467343c426a2
TakagiLia <r.takagi.test@gmail.com>
2023-09-02 16:51:36 +0900

    ログアウト時の確認ダイアログの実装、LogoutDialog.ktの追加

---
+++ app/src/main/java/com/example/makeyourbody/LogoutDialog.kt
@@ -0,0 +1,34 @@
+package com.example.makeyourbody
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+
+class LogoutDialog : DialogFragment() {
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        return activity?.let {
+
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage(R.string.dialog_title_logout)
+                .setPositiveButton(
+                    R.string.dialog_yes
+                ) { _, _ ->
+                    Log.d("--success--", "ログアウトする")
+                    NiftyCloudApiClient().logOut()
+                    findNavController().navigate(R.id.action_login_screen)
+                }
+                .setNegativeButton(
+                    R.string.dialog_no
+                ) { _, _ ->
+                    Log.d("--success--", "ログアウトしない")
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
